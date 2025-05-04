6cec3628ad3e7d20c171cbd88e9d23269a5a01b5
ike <pondfield.dev@gmail.com>
2025-05-04 16:36:42 +0900

    H2DBへのテーブル定義、投入データファイル作成

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('アップデート対応', 'Spring最新化アップデート対応');
+insert into issues (summary, description) values ('データグラフ、比較画面が欲しい', 'データをグラフ化し、他のデータと比較して分析する画面が欲しい');
+insert into issues (summary, description) values ('進捗表や課題関連の図が欲しい', '個別の課題と、後続タスクの管理を助けるWBSのような機能が欲しい');
